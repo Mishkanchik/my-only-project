4e634d9be82975e07499f2ce69c4cd3dc62dc4b6
mishkanchik <csprince666@gmail.com>
2022-05-15 16:13:38 +0300

    commit  header in three main

---
+++ script.js
@@ -0,0 +1,12 @@
+$('.slider-welcome').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay:true,
+    dots:true,
+    arrows:false,
+  });
+
+  $('.slider-destination').slick({
+    autoplay:true,
+  });
